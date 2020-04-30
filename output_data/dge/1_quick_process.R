a42989f90a4cfadd212dde129ec408d1f8014d80
marianarf <marianar@valiot.io>
2020-04-29 22:33:09 -0500

    Add gif

--- output_data/dge/1_quick_process.R
+++ output_data/dge/1_quick_process.R
@@ -29,9 +29,15 @@ levels(as.factor(dat$FECHA_CORTE))
 levels(as.factor(dat$FECHA_ACTUALIZACION))
 nrow((dat))
 
-dd <- dat %>% select(ID_REGISTRO, ENTIDAD_UM, ENTIDAD_RES, FECHA_INGRESO, FECHA_SINTOMAS, FECHA_DEF, RESULTADO, MUNICIPIO_RES) %>%
+dd_mun <- dat %>% select(ID_REGISTRO, ENTIDAD_UM, ENTIDAD_RES, FECHA_INGRESO, FECHA_SINTOMAS, FECHA_DEF, RESULTADO, MUNICIPIO_RES) %>%
   filter(as.integer(RESULTADO)==1) %>%
   mutate(., FECHA_INGRESO = as.POSIXct(FECHA_INGRESO, format='%Y-%m-%d')) %>%
   arrange(desc(FECHA_INGRESO))
 
+dd <- dat %>% select(ID_REGISTRO, ENTIDAD_UM, ENTIDAD_RES, FECHA_INGRESO, FECHA_SINTOMAS, FECHA_DEF, RESULTADO, ) %>%
+  filter(as.integer(RESULTADO)==1) %>%
+  mutate(., FECHA_INGRESO = as.POSIXct(FECHA_INGRESO, format='%Y-%m-%d')) %>%
+  arrange(desc(FECHA_INGRESO))
+
+write_csv(dd_mun, 'filter_mun.csv')
 write_csv(dd, 'filter.csv')
